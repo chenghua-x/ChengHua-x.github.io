1040eb1acd743a2c3069984e23c11474a7da3b42
unknown <635592341@qq.com>
2021-03-11 19:15:03 +0800

    Site updated: 2021-03-11 19:14:52

---
+++ dist/musics.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    
+    autoplay: false,
+    audio: [{
+        name: '白月光与朱砂痣',
+        artist: '聆听音乐',
+        url: 'http://mp.333ttt.com/mp3music/161640388.mp3',
+        cover: 'https://img.imgdb.cn/item/604874115aedab222c3b8faa.jpg',
+    }]
+});
